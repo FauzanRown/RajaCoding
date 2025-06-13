90f82508115042c5504b4e2c314f3fa46a445432
Fanan <ahmadbantul3@gmail.com>
2025-06-13 13:24:01 +0700

    add table category

--- database/db.sql
+++ database/db.sql
@@ -13,6 +13,14 @@ CREATE TABLE `user` (
   UNIQUE KEY `user_unique` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 -- log_book.pembimbing definition
+-- log_book.category definition
+
+CREATE TABLE `category` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `pembimbing` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
@@ -26,18 +34,22 @@ CREATE TABLE `pembimbing` (
   CONSTRAINT `mahasiswa_FK` FOREIGN KEY (`mahasiswa_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 -- log_book.jurnal definition
 
 CREATE TABLE `jurnal` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `id_pembimbing` bigint unsigned NOT NULL,
+  `pembimbing_id` bigint unsigned NOT NULL,
+  `category_id` bigint unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
   `note` text NOT NULL,
   `revision` text,
   `status` enum('valid','tidak valid','belum di review') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'belum di review',
   `created_at` timestamp NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `jurnal_pembimbing_FK` (`id_pembimbing`),
-  CONSTRAINT `jurnal_pembimbing_FK` FOREIGN KEY (`id_pembimbing`) REFERENCES `pembimbing` (`id`)
+  KEY `jurnal_category_FK` (`category_id`),
+  KEY `jurnal_pembimbing_FK` (`pembimbing_id`),
+  CONSTRAINT `jurnal_category_FK` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `jurnal_pembimbing_FK` FOREIGN KEY (`pembimbing_id`) REFERENCES `pembimbing` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE VIEW view_jurnal_category AS SELECT jurnal.*, category.name AS category_name,  FROM jurnal JOIN category ON jurnal.category_id = category.id;
