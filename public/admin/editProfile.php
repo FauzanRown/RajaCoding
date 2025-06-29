501c70b0d718be27e49110433d1398cf2f95ff12
Fanan <ahmadbantul3@gmail.com>
2025-06-29 20:30:57 +0700

    add admin page

---
+++ public/admin/editProfile.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+if (!isset($_SESSION["login"])) {
+  header("Location: ../login.php");
+  exit;
+}
+if ($_SESSION['role'] !== 'admin') {
+  header("Location: ../403.php");
+  exit;
+}
+require_once '../../controller/userController.php';
+$successMsg = $_SESSION["success"] ?? null;
+unset($_SESSION["success"], $_SESSION["error"]);
+if (isset($_POST["submit"])) {
+  if (update($_SESSION["id"]) > 0) {
+    $_SESSION["success"] = "Berhasil Update Profile";
+    $successMsg = $_SESSION["success"];
+    header("Location: editProfile.php");
+  } else {
+    $_SESSION["error"] = "Gagal Update Profile";
+    $errorMsg = $_SESSION["error"];
+    unset($_SESSION["error"]);
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dashboard | Edit Profile</title>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../../assets/css/dashboard.css" />
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+</head>
+
+<body>
+  <div class="container">
+    <?php include '../component/admin/sidebar.php' ?>
+    <div id="content">
+      <div class="form-container  ">
+        <h1>PROFILE</h1>
+        <img class="text-center mx-auto rounded-circle" style="width: 128px; display:block" src="<?= $userData['image'] == "user.png" ? "../../assets/img/user.png" : "../images/" . $userData['image'] ?>" alt="" />
+        <div class="form-container">
+          <div class="form-flex">
+            <div class="form-group">
+              <label>Nama :</label>
+              <p class=" text-start"><?= $userData["name"] ?></p>
+            </div>
+            <div class="form-group">
+              <label>Email :</label>
+              <p class=" text-start"><?= $userData["email"] ?></p>
+            </div>
+          </div>
+          <hr>
+          <h1 class="text-start mb-2">Update Profile : </h1>
+          <form method="post" action="" enctype="multipart/form-data">
+            <input type="hidden" value="<?= $userData["image"] ?>" name="gambarLama" />
+            <div class="form-flex">
+              <div class="form-group">
+                <label for="name">Nama</label>
+                <input type="text" placeholder="Masukkan Nama Anda" name="name" id="name" value="<?= $userData["name"] ?>" />
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" placeholder="Masukkan Email Anda" name="email" id="email" value="<?= $userData["email"] ?>" />
+              </div>
+            </div>
+            <label for="img">Foto Profile</label>
+            <input type="file" class=" form-control" name="img" id="img" />
+            <div class="submit-button ">
+              <button type="submit" name="submit">Update</button>
+            </div>
+          </form>
+        </div>
+
+        <p class="copyright">
+          All Rights Reserved • Copyright StudentLogbook by RajaCoding 2025 in
+          Yogyakarta
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <script src="../../assets/js/RoleUser/tambahjournal.js"></script>
+  <!-- Toastr JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+  <script>
+    <?php if (!empty($successMsg)) : ?>
+      toastr.success("<?= $successMsg ?>");
+    <?php endif; ?>
+
+    <?php if (!empty($errorMsg)) : ?>
+      toastr.error("<?= $errorMsg ?>");
+    <?php endif; ?>
+  </script>
+</body>
+
+</html>
---
+++ public/component/admin/modalUpdateUser.php
@@ -0,0 +1,47 @@
+  <!-- modal  tambah tempat tugas-->
+
+  <div class="modal fade modal-lg" id="<?= 'modalUpdateUser'.$row[0] ?>" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Masukan Data User</h4>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form method="POST" class="w-100" enctype="multipart/form-data">
+          <div class="modal-body">
+            <input type="hidden" value="<?= $row[0] ?>" name="user_id" />
+            <input type="hidden" value="<?= $row[4] ?>" name="gambarLama" />
+            <div class="form-group w-100">
+              <label>Nama User</label>
+              <input type="text" value="<?= $row[1] ?>" placeholder="Username" name="name" />
+              <label class="mt-2">Email User</label>
+              <input type="email" value="<?= $row[2] ?>" placeholder="Email" name="email" />
+              <label class="mt-2">Image User</label>
+              <input class="form-control" type="file" name="img">
+              <label class="mt-2">Role User</label>
+              <select name="role">
+                <option value="">Pilih Role</option>
+                <option value="admin" <?= ($row[3] == 'admin') ? 'selected' : '' ?>>Admin</option>
+                <option value="dosen" <?= ($row[3] == 'dosen') ? 'selected' : '' ?>>Dosen</option>
+                <option value="mahasiswa" <?= ($row[3] == 'mahasiswa') ? 'selected' : '' ?>>Mahasiswa</option>
+              </select>
+              <span style="color:red;"></span><br>
+              <label class="mt-2">Password User</label>
+              <input type="password" placeholder="Password" name="password" />
+              <label class="mt-2">Confirm Password</label>
+              <input type="password" placeholder="Confirm Password" name="confirm_password" />
+              <!-- <select name="kateogri" style="width: 100%;" id="kateogri"></select> -->
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button class="btn btn-primary" type="submit" name="update">Submit</button>
+
+          </div>
+        </form>
+
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
